62433a153e67794fae8dfac9a76e1b72c82abe9e
minhhieu3110 <phamminhhieu599@gmail.com>
2024-08-06 10:11:05 +0700

    Update 1

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,20 +1,29 @@
 import '../Style/Home.css'
 import {Outlet} from "react-router";
+import {Link} from "react-router-dom";
 export default function Home(){
     return (
         <div>
             <div className="header">
-            
+                <nav>
+                    <ul>
+                        {/*<li>Hello {location.name}</li>*/}
+                        <li><Link to='/home'>Home</Link></li>
+                        <li><Link to='orders'>Orders</Link></li>
+                        <li><Link to='/admin'>Admin</Link></li>
+                        <li><Link to='/'>Logout</Link></li>
+                    </ul>
+                </nav>
             </div>
-                <div className="logo-banner">
-                    <div className="logo">Logo</div>
-                    <div className="banner">Banner</div>
+            <div className="logo-banner">
+                <div className="logo">Logo</div>
+                <div className="banner">Banner</div>
+            </div>
+            <div className="nav">
+                <div className="left">
+                    <div className="home">Home</div>
+                    <div className="product">Product</div>
                 </div>
-                <div className="nav">
-                    <div className="left">
-                        <div className="home">Home</div>
-                        <div className="product">Product</div>
-                    </div>
                     <div className="right">
                         <div className="order">Order</div>
                     </div>
